56fffff7907e709ef5baa641336ebedfdaab9acc
Josh Koszarek <112987840+joshkoszarek@users.noreply.github.com.>
2024-05-17 17:46:32 -0500

    Add function to repeat a string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(word, numberOfTimesToRepeatWord) {
+    let totalString  = ''
+    if (numberOfTimesToRepeatWord < 0) { 
+        return 'ERROR';
+    }
+    for (let i = 0; i < numberOfTimesToRepeatWord; i++){ 
+        totalString += word;
+    }
+    return totalString; 
 };
 
 // Do not edit below this line
