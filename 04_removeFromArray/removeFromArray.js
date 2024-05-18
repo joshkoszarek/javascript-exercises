afe37076b973759764907e15edc49579b575efdc
Josh Koszarek <112987840+joshkoszarek@users.noreply.github.com.>
2024-05-17 20:45:47 -0500

    Add function to remove item from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,22 @@
-const removeFromArray = function() {
+
+const removeFromArray = function(arrayToCheck, ...thingsToRemoveFromArray) {
+    let thingToCheckFor = undefined;
+    for (let j = 0; j < thingsToRemoveFromArray.length; j++) { 
+        thingToCheckFor = thingsToRemoveFromArray[j]
+        for (let i = 0; i < arrayToCheck.length; i++){ 
+            if (arrayToCheck[i] === thingToCheckFor){ 
+                arrayToCheck.splice(i, 1); 
+                i -= 1 
+            }
+
+            }
+        }
+    return arrayToCheck;  
+    
 
 };
+
+removeFromArray([1,2,2,4], 2);
 
 // Do not edit below this line
 module.exports = removeFromArray;
