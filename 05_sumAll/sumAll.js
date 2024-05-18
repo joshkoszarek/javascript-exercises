3f31cd32260fe55c3e7f03f158e9330b9c9c82c0
Josh Koszarek <112987840+joshkoszarek@users.noreply.github.com.>
2024-05-17 21:50:40 -0500

    Add function to sum between two integers

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+function getSum(number) { 
+    return (((number)*(number + 1))/2);
+}
 
+const sumAll = function(startValue, endValue) {
+    if (typeof startValue != 'number' || typeof endValue != 'number'|| startValue < 0 || endValue < 0){
+        return 'ERROR'
+    } 
+    let startCountAt = startValue; 
+    let endCountAt = endValue;
+    if (startValue > endValue){ 
+        endCountAt = startValue;
+        startCountAt = endValue;
+    }
+    let totalSum = 0;
+    for (let i = startCountAt; i <= endCountAt; i++){ 
+        totalSum += i; 
+    }
+    return totalSum; 
 };
+console.log(sumAll(1,4)); 
 
 // Do not edit below this line
 module.exports = sumAll;
